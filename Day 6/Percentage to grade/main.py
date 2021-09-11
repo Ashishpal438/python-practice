e0be7330663dddd7c9ef9a2e9246b13ec2c4c1da
Ashish kumar Pal <ashishpal438@gmail.com>
2021-09-11 23:33:44 +0530

    Program to accept percentage of a student and display grade.

---
+++ Day 6/Percentage to grade/main.py
@@ -0,0 +1,11 @@
+perc = float(input("Enter percentage of a student : "))
+if perc > 85:
+    print("A")
+elif perc > 70:
+    print("B")    
+elif perc > 60:
+    print("C")    
+elif perc > 45:
+    print("D")
+else:
+    print("E")    
